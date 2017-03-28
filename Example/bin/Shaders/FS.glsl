b37e5d718291f30ba0cb220170aa1701be128832
Chemonky <luricardorod@gmail.com>
2017-03-28 16:16:00 -0600

    phon y blinn

--- Example/bin/Shaders/FS.glsl
+++ Example/bin/Shaders/FS.glsl
@@ -26,14 +26,13 @@ void main(){
 	lowp vec3 globalLight = vec3(0, 0, 0);
 	lowp vec3 pointLight = vec3(0, 0, 0);
 	lowp vec3 diffuseLight = vec3(0, 0, 0);
-	highp vec3 colorDiffuse = vec3(.2,0, 0);;
+	highp vec3 colorDiffuse = vec3(1, 1, 1);
 
 #ifdef	USE_DIFFUSE
-	lightIntensity = pow(dot(normalize(positionCamera.xyz - vert.xyz),reflect(normalize(vert.xyz - posPoint.xyz), vec3(0,1,0))),10.0);
-	if (lightIntensity > 1.0)
-		lightIntensity = 1.0;
-	else if (lightIntensity < 0.0)
-		lightIntensity = 0.0;
+	lightIntensity = pow(dot(normalize(positionCamera.xyz - vert.xyz),normalize(reflect((vert.xyz - posPoint.xyz), vecTransformed.xyz)))*0.5+0.5,10.0);
+	lightIntensity = pow(dot(normalize(posPoint.xyz - vert.xyz + positionCamera.xyz - vert.xyz),vecTransformed.xyz),10.0);
+	
+	clamp(lightIntensity, 0.0, 1.0);
 	diffuseLight = lightIntensity*colorDiffuse;
 
 #endif
@@ -58,7 +57,7 @@ void main(){
 	pointLight = lightIntensity*colorPoint;
 #endif
 #if defined(USE_TEXCOORD0) 
-	vector = texture2D(diffuse,vecUVCoords).rgb*.1 +texture2D(diffuse,vecUVCoords).rgb*pointLight + texture2D(diffuse,vecUVCoords).rgb*globalLight + texture2D(diffuse,vecUVCoords).rgb*diffuseLight*10.0;
+	vector = texture2D(diffuse,vecUVCoords).rgb*.1 +texture2D(diffuse,vecUVCoords).rgb*pointLight + texture2D(diffuse,vecUVCoords).rgb*globalLight + texture2D(diffuse,vecUVCoords).rgb*diffuseLight;
 #else
 	vector = normalize(vec3(vecTransformed)*0.5 + 0.5)*.3 +normalize(vec3(vecTransformed)*0.5 + 0.5)*pointLight + normalize(vec3(vecTransformed)*0.5 + 0.5)*globalLight;
 #endif
