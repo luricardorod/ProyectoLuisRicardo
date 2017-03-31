183d449ec729da87e2138baf304747776ae0ffd0
0Camus0 <camus_mm@hotmail.com>
2017-03-31 01:14:02 -0600

    - Quité varyings que no se necesitan, son uniforms, los tienes a la mano en el vertex y en el pixel, no es necesario pasarlos de uno a otro, eso sólo es para algo que quieras interpolar entre vertex y pixel como una normal, tangente, posicion, algo del vertice.

--- Example/bin/Shaders/FS.glsl
+++ Example/bin/Shaders/FS.glsl
@@ -1,32 +1,30 @@
 uniform highp sampler2D diffuse;
 uniform highp sampler2D normalLoc;
 
+#ifdef USE_GLOBALLIGHT
+uniform highp vec3 DirectionGlobalLight;
+uniform highp vec3 ColorGlobalLight;
+#endif
 
-varying highp vec3 vecTransformed;
+#ifdef	USE_POINTLIGHT
+uniform highp vec3 PositionPointLight;
+uniform highp vec3 ColorPointLight;
+#endif
+
+#ifdef	USE_DIFFUSE
+uniform highp vec3 PositionCamera;
+#endif
+
+
 varying highp vec4 vert;
 
 #ifdef USE_TEXCOORD0
 varying highp vec2 vecUVCoords;
 #endif
-#ifdef USE_GLOBALLIGHT
-varying highp vec3 light;
-varying highp vec3 color;
-#endif
-
-#ifdef	USE_DIFFUSE
-varying highp vec3 positionCamera;
-#endif
-
-#ifdef	USE_POINTLIGHT
-varying highp vec3 posPoint;
-varying highp vec3 colorPoint;
-#endif
 
 varying highp vec3 binormal1;
 varying highp vec3 tangente1;
 varying highp vec3 normal1;
-varying  highp mat3 world3all;
-highp vec3 lucas;
 
 
 
@@ -36,7 +34,7 @@ void main(){
 	ejes[0] = normalize(tangente1);
 	ejes[1] = normalize(binormal1);
 	ejes[2] = normalize(normal1);
-	lucas = texture2D(normalLoc,vecUVCoords).rgb * 2.0 - 1.0;
+	highp vec3 lucas = texture2D(normalLoc,vecUVCoords).rgb * 2.0 - 1.0;
 	lucas.g = -lucas.g;
 	lucas = normalize(ejes*lucas);
 	highp float lightIntensity;
@@ -47,8 +45,8 @@ void main(){
 	highp vec3 colorDiffuse = vec3(1, 1, 1);
 
 #ifdef	USE_DIFFUSE
-	lightIntensity = pow(dot(normalize(positionCamera.xyz - vert.xyz),normalize(reflect((vert.xyz - posPoint.xyz), lucas.xyz)))*0.5+0.5,10.0);
-	//lightIntensity = pow(dot(normalize(posPoint.xyz - vert.xyz + positionCamera.xyz - vert.xyz),lucas.xyz)*0.5+0.5,10.0);
+	lightIntensity = pow(dot(normalize(PositionCamera.xyz - vert.xyz),normalize(reflect((vert.xyz - PositionPointLight.xyz), lucas.xyz)))*0.5+0.5,10.0);
+	//lightIntensity = pow(dot(normalize(PositionPointLight.xyz - vert.xyz + PositionCamera.xyz - vert.xyz),lucas.xyz)*0.5+0.5,10.0);
 	
 	clamp(lightIntensity, 0.0, 1.0);
 	diffuseLight = lightIntensity*colorDiffuse;
@@ -56,28 +54,27 @@ void main(){
 #endif
 
 #if defined(USE_GLOBALLIGHT)
-	lightIntensity = dot(normalize(light.xyz), normalize(lucas.xyz));
+	lightIntensity = dot(normalize(DirectionGlobalLight.xyz), normalize(lucas.xyz));
 	if (lightIntensity > 1.0)
 		lightIntensity = 1.0;
 	else if (lightIntensity < 0.0)
 		lightIntensity = 0.0;
-	globalLight = lightIntensity*color;
+	globalLight = lightIntensity*ColorGlobalLight;
 #endif
 
 #if defined(USE_POINTLIGHT)
 	lightIntensity = 0.0;
-	lightIntensity = dot( normalize(posPoint.xyz - vert.xyz ), normalize(lucas.xyz));
-	lightIntensity = lightIntensity*(100.0 / length(posPoint.xyz - vert.xyz));
+	lightIntensity = dot( normalize(PositionPointLight.xyz - vert.xyz ), normalize(lucas.xyz));
+	lightIntensity = lightIntensity*(100.0 / length(PositionPointLight.xyz - vert.xyz));
 	if (lightIntensity > 1.0)
 		lightIntensity = 1.0;
 	else if (lightIntensity < 0.0)
 		lightIntensity = 0.0;
-	pointLight = lightIntensity*colorPoint;
+	pointLight = lightIntensity*ColorPointLight;
 #endif
 #if defined(USE_TEXCOORD0) 
 	vector = texture2D(diffuse,vecUVCoords).rgb*.1 +texture2D(diffuse,vecUVCoords).rgb*pointLight + texture2D(diffuse,vecUVCoords).rgb*globalLight + texture2D(diffuse,vecUVCoords).rgb*diffuseLight;
 
-	//vector = (normalize(vec3(lucas)) *0.5 + 0.5);// +  (normalize(vec3(vecTransformed)) *0.5 + 0.5) * pointLight+(normalize(vec3(vecTransformed)))*globalLight;
 #endif
 	gl_FragColor = vec4(vector,1.0);
 }
--- Example/bin/Shaders/VS.glsl
+++ Example/bin/Shaders/VS.glsl
@@ -7,36 +7,15 @@ attribute highp vec4 Binormal;
 attribute highp vec4 Tangente;
 
 
-varying highp vec3 vecTransformed;
 varying highp vec4 vert;
-
 
 #ifdef USE_TEXCOORD0
 varying highp vec2 vecUVCoords;
 #endif
 
-#ifdef USE_GLOBALLIGHT
-uniform highp vec3 DirectionGlobalLight;
-uniform highp vec3 ColorGlobalLight;
-varying highp vec3 light;
-varying highp vec3 color;
-#endif
-
-#ifdef	USE_POINTLIGHT
-uniform highp vec3 PositionPointLight;
-uniform highp vec3 ColorPointLight;
-varying highp vec3 posPoint;
-varying highp vec3 colorPoint;
-#endif
-
-#ifdef	USE_DIFFUSE
-uniform highp vec3 PositionCamera; 
-varying highp vec3 positionCamera;
-#endif
-
 uniform highp mat4 WVP;
 uniform highp mat4 World;
-highp mat3 World3;
+
 
 
 varying highp vec3 binormal1;
@@ -46,6 +25,8 @@ varying  highp mat3 world3all;
 
 void main(){
 
+highp mat3 World3;
+
 World3[0] = World[0].xyz;
 World3[1] = World[1].xyz;
 World3[2] = World[2].xyz;
@@ -53,22 +34,12 @@ World3[2] = World[2].xyz;
 binormal1 = normalize(World3 *Binormal.xyz);
 tangente1 = normalize(World3 *Tangente.xyz);
 normal1 = normalize(World3 *Normal.xyz);
-world3all = World3;
-#ifdef USE_GLOBALLIGHT
-	light = DirectionGlobalLight;
-	color = ColorGlobalLight;
-#endif
-#ifdef USE_POINTLIGHT
-	posPoint = PositionPointLight;
-	colorPoint = ColorPointLight;
-#endif
+
+
 #ifdef USE_TEXCOORD0
 	vecUVCoords = UV;
 #endif
-#ifdef USE_DIFFUSE
-	positionCamera = PositionCamera;
-#endif
-	vecTransformed	= normalize(World3 *Normal.xyz);
+
 	vert = World*Vertex;
 	gl_Position = WVP*Vertex;
 }
